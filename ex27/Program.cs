b6a343a16fa7fedd03e09f4de130dca1f2139377
BuMax <max@butenko.in>
2022-12-10 23:13:37 +1000

    Exercise #27 completed

---
+++ ex27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+System.Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+
+int result = new int();
+
+while (number >= 1)
+{
+    result += number % 10;
+    number /= 10;
+}
+
+System.Console.WriteLine($"Сумма всех чисел числа равна {result}");
